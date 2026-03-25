fe4e652904f5650037bccb0c7c4dd0f73061c40f
Darryl Yeo <dev@darryl-yeo.com>
2026-03-25 00:15:38 -0700

    Polyfills: set up shims

---
+++ src/polyfills.ts
@@ -0,0 +1,4 @@
+void (await import('map.prototype.getorinsertcomputed/shim')).default()
+void (await import('weakmap.prototype.getorinsertcomputed/shim')).default()
+
+export {}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	// Polyfills
+	import '$/polyfills.ts'
 
-	let { children } = $props();
+
+	// State
+	let {
+		children,
+	} = $props()
+
+
+	// Components
+	import favicon from '$/assets/favicon.svg'
 </script>
 
+
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link
+		rel="icon"
+		href={favicon}
+	/>
 </svelte:head>
+
 
 {@render children()}
