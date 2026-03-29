7b608386564be9b2b59a319cf553f7d4ee95d0ea
Darryl Yeo <dev@darryl-yeo.com>
2026-03-29 01:44:13 -0700

    Components: update `<Collapsible>`

---
+++ src/components/Collapsible.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	// State
+	import type { WithRest } from '$/typescript/WithRest.ts'
+	import type { Snippet } from 'svelte'
+	import type { SvelteHTMLElements } from 'svelte/elements'
+
+	let {
+		open = $bindable(true),
+		ontoggle,
+		onclose,
+
+		Summary,
+		Toolbar,
+		Annotation,
+		children,
+
+		...detailsProps
+	}: WithRest<
+		{
+			open?: boolean
+			ontoggle?: (e: Event) => void
+			onclose?: (id?: typeof detailsProps.id) => void
+
+			Annotation?: Snippet<[{
+				open: boolean,
+			}]>
+			Toolbar?: Snippet<[{
+				open: boolean,
+			}]>
+			Summary?: Snippet<[{
+				open: boolean,
+			}]>
+			children?: Snippet<[{
+				open: boolean,
+			}]>
+		},
+		SvelteHTMLElements['details']
+	> = $props()
+
+
+	// Inner context
+	import { incrementHeadingLevel } from '$/context/headingLevel.ts'
+
+	incrementHeadingLevel()
+</script>
+
+
+<details
+	bind:open
+	ontoggle={(e) => {
+		if (!e.currentTarget.open && onclose) {
+			setTimeout(() => onclose(detailsProps.id), 300)
+		}
+		ontoggle?.(e)
+	}}
+	data-scroll-container="block snap-block"
+	{...detailsProps}
+>
+	<summary data-sticky>
+		<div
+			data-row-item="flexible"
+			data-row="align-center wrap"
+		>
+			{#if Summary}
+				{@render Summary({
+					open,
+				})}
+			{/if}
+
+			{#if Toolbar || Annotation}
+				<div data-row="wrap">
+					{#if Toolbar}
+						{@render Toolbar({
+							open,
+						})}
+					{/if}
+
+					{#if Annotation}
+						{@render Annotation({
+							open,
+						})}
+					{/if}
+				</div>
+			{/if}
+		</div>
+	</summary>
+
+	{#if children}
+		<div data-column>
+			{@render children({
+				open,
+			})}
+		</div>
+	{/if}
+</details>
