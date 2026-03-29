df936980e63f67c35f38554d8183ec1b1b4ecca2
Darryl Yeo <dev@darryl-yeo.com>
2026-03-29 15:05:38 -0700

    Components: update `<ParentPageCollapsible>`

---
+++ src/components/ParentPageCollapsible.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	// Types/constants
+	import type { ResolvedPathname } from '$app/types'
+
+
+	// Context
+	import {
+		getOnNestedCollapsibleClose,
+		setOnNestedCollapsibleClose,
+	} from '$/context/onNestedCollapsibleClose.ts'
+
+	const onNestedCollapsibleClose = getOnNestedCollapsibleClose()
+
+
+	// State
+	import type { WithRest } from '$/typescript/WithRest.ts'
+	import type { ComponentProps, Snippet } from 'svelte'
+
+	let {
+		title,
+		href,
+
+		open = $bindable(true),
+		ontoggle,
+
+		Summary,
+		children,
+
+		...CollapsibleProps
+	}: WithRest<
+		{
+			title: string
+			href?: ResolvedPathname
+
+			open?: boolean
+			ontoggle?: (e: Event) => void
+
+			Summary?: Snippet<[{
+				open: boolean,
+			}]>
+			children?: Snippet<[{
+				open: boolean,
+			}]>
+		},
+		ComponentProps<typeof Collapsible>
+	> = $props()
+
+
+	// Inner context
+	import { goto } from '$app/navigation'
+
+	setOnNestedCollapsibleClose((collapsibleId?: string) => {
+		if(href)
+			goto(
+				collapsibleId ?
+					`${href.replace(/#.*$/, '')}#${encodeURIComponent(collapsibleId)}`
+				:
+					href
+			)
+	})
+
+
+	// Components
+	import Collapsible from '$/components/Collapsible.svelte'
+	import Heading from '$/components/Heading.svelte'
+</script>
+
+
+<Collapsible
+	bind:open
+	{ontoggle}
+	onclose={id => onNestedCollapsibleClose?.(id ?? undefined)}
+	data-card
+	{...CollapsibleProps}
+>
+	{#snippet Summary({
+		open,
+	})}
+		{#if Summary}
+			{@render Summary({
+				open,
+			})}
+		{:else}
+			<Heading>
+				{#if href}
+					<a
+						data-link
+						href={href}
+					>{title}</a>
+				{:else}
+					{title}
+				{/if}
+			</Heading>
+		{/if}
+	{/snippet}
+
+	{#snippet children({
+		open,
+	})}
+		{#if children}
+			{@render children({
+				open,
+			})}
+		{/if}
+	{/snippet}
+</Collapsible>
