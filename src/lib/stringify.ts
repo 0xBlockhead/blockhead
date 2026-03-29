9b7313a3109688cf7ce46274b7b79b0b1cbe037b
Darryl Yeo <dev@darryl-yeo.com>
2026-03-29 01:12:40 -0700

    Repository: update `stringify`

---
+++ src/lib/stringify.ts
@@ -0,0 +1,18 @@
+export const stringify: typeof JSON.stringify = (
+	value,
+	replacer,
+	space
+) => (
+	JSON.stringify(
+		value,
+		(key, val) => (
+			typeof val === 'bigint' ?
+				val.toString()
+			: typeof replacer === 'function' ?
+				replacer.call(value, key, val)
+			:
+				val
+		),
+		space,
+	)
+)
