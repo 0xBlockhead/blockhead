400bf71b0ef94899434b7e3f2f604da6b6571c01
Darryl Yeo <dev@darryl-yeo.com>
2026-03-29 00:34:17 -0700

    Repository: update `useContext`

---
+++ src/context/useContext.ts
@@ -0,0 +1,27 @@
+import { getContext, hasContext, setContext } from 'svelte'
+
+export type ContextKey = string | symbol
+
+export function useContext<_Value>(key: ContextKey, getInitialValue?: () => _Value): _Value
+export function useContext<_Value>(key: ContextKey): _Value | undefined
+export function useContext<_Value>(
+	key: ContextKey,
+	getInitialValue?: () => _Value,
+): _Value | undefined {
+	return (
+		hasContext(key) ?
+			getContext<_Value>(key)
+		: getInitialValue ?
+			setContext(key, getInitialValue())
+		:
+			undefined
+	)
+}
+
+export const useGetSetContext = <_Value>(
+	key: ContextKey,
+	getInitialValue?: () => _Value,
+) => ({
+	get: () => useContext<_Value>(key, getInitialValue),
+	set: (value: _Value) => setContext(key, value),
+})
