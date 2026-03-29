c1bae123f89bb9bebf079e436b4d4eb36d31df1f
Darryl Yeo <dev@darryl-yeo.com>
2026-03-29 00:32:15 -0700

    Root: set up layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,14 @@
 	import '$/styles/components.css'
 
 
+	// Context
+	import { useNavigationItems } from './navigationItems.svelte'
+
+	const navigationItems = $derived(
+		useNavigationItems().navigationItems
+	)
+
+
 	// State
 	let {
 		children,
@@ -17,6 +25,8 @@
 
 
 	// Components
+	import Boundary from '$/components/Boundary.svelte'
+	import Navigation from './Navigation.svelte'
 	import { asset } from '$app/paths'
 </script>
 
@@ -29,4 +39,131 @@
 </svelte:head>
 
 
-{@render children()}
+<div
+	id="layout"
+	data-scroll-container="layout-inline-panes snap-inline"
+	data-sticky-container
+>
+	<a
+		href="#main"
+		class="skip-link"
+	>
+		Skip to main content
+	</a>
+
+	<Navigation
+		{navigationItems}
+	/>
+
+	<div
+		id="main"
+		tabindex="-1"
+		data-scroll-item="pane-flexible"
+		data-sticky-container
+		data-column="layout-flex"
+	>
+		<Boundary>
+			{#snippet Pending()}
+				<main
+					data-column
+					class="layout-boundary-loading"
+				>
+					<p data-text="muted">Loading…</p>
+				</main>
+			{/snippet}
+
+			<div
+				id="layout"
+				data-column
+			>
+				{@render children()}
+			</div>
+		</Boundary>
+	</div>
+</div>
+
+
+<style>
+	#layout {
+		--navigation-desktop-inlineSize: 21rem;
+		--navigation-mobile-blockSize: 4rem;
+
+		inline-size: 100dvw;
+		block-size: 100dvh;
+		padding: var(--safeArea-insetTop) var(--safeArea-insetRight) var(--safeArea-insetBottom) var(--safeArea-insetLeft);
+		align-items: start;
+		gap: var(--separator-width);
+
+		&[data-scroll-container] {
+			--sticky-paddingBlockStart: var(--safeArea-insetTop);
+			--sticky-paddingBlockEnd: var(--safeArea-insetBottom);
+			--sticky-paddingInlineStart: var(--safeArea-insetLeft);
+			--sticky-paddingInlineEnd: var(--safeArea-insetRight);
+		}
+
+		@media (width >= 60rem) {
+			&[data-scroll-container~='layout-inline-panes'] {
+				--scrollPanes-paneStatic-inlineSize: var(--navigation-desktop-inlineSize);
+			}
+		}
+
+		> .layout-nav :global(nav) {
+			box-shadow: 0 0 0 var(--separator-width) var(--border-color);
+		}
+
+		> #main {
+			--sticky-paddingInlineStart: clamp(1rem, 6cqi, 2rem);
+			--sticky-paddingInlineEnd: clamp(1rem, 6cqi, 2rem);
+
+			align-self: stretch;
+
+			> #layout {
+				view-transition-name: Main;
+
+				min-height: 100%;	
+
+				padding: 1.5rem;
+
+				> :global([data-scroll-container]:only-child) {
+					--scrollContainer-sizeBlock: calc(100cqb - 3rem);
+				}
+			}
+		}
+	}
+
+
+	.skip-link {
+		position: absolute;
+		top: -100%;
+		left: 0;
+		padding: 0.5em 1em;
+		background: var(--color-bg-page);
+		z-index: 1000;
+		color: var(--color-fg);
+
+		&:focus {
+			top: 0;
+		}
+	}
+
+	::view-transition-old(Main) {
+		animation: 0.2s var(--transition-easeOutExpo) both MainTransitionOut;
+	}
+	::view-transition-new(Main) {
+		animation: 0.2s var(--transition-easeOutExpo) both MainTransitionIn;
+	}
+	@keyframes MainTransitionIn {
+		from {
+			opacity: 0;
+			scale: 0.95;
+			filter: blur(2px);
+		}
+	}
+	@keyframes MainTransitionOut {
+		to {
+			opacity: 0;
+			scale: 0.95;
+			filter: blur(2px);
+		}
+	}
+</style>
