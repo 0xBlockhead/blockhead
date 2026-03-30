b3b5c47d1e138549783b0adb633bcd0b808ce5ef
Darryl Yeo <dev@darryl-yeo.com>
2026-03-29 17:35:58 -0700

    TypeScript: `Array`

---
+++ src/typescript/Array.d.ts
@@ -0,0 +1,10 @@
+interface Array<T> {
+	filter<
+		_DefinedItem extends Exclude<T, undefined>
+	>(
+		predicate: (value: T, index: number, array: T[]) => value is _DefinedItem,
+		thisArg?: unknown,
+	): (
+		_DefinedItem[]
+	)
+}
