0bdf032b338d8986b3fe8be1b9b770f3bbf1f485
Darryl Yeo <dev@darryl-yeo.com>
2026-08-02 13:41:04 -0700

    Sources / Juno: use the typed Starknet query contract

--- src/sources/Juno/JsonRpc/queries.ts
+++ src/sources/Juno/JsonRpc/queries.ts
@@ -1,21 +1,5 @@
-import type { SourceBinding } from '$/sources/SourceBinding.ts'
-import { jsonRpc2 } from '$/sources/_shared/wire/JsonRpc2/client.ts'
-import type { JsonValue } from '$/typescript/JsonValue.ts'
+import bindings from '$/sources/Juno/bindings.ts'
+import { Source } from '$/sources/Source.ts'
+import { starknetJsonRpc } from '$/sources/_shared/interfaces/StarknetJsonRpc/queries.ts'
 
-export const request = (
-	binding: SourceBinding,
-	method: string,
-	params?: readonly JsonValue[]
-) => jsonRpc2<JsonValue>(binding, method, params)
-
-export const getBlockNumber = (binding: SourceBinding) => (
-	request(binding, 'starknet_blockNumber')
-)
-
-export const getChainId = (binding: SourceBinding) => (
-	request(binding, 'starknet_chainId')
-)
-
-export const getSyncing = (binding: SourceBinding) => (
-	request(binding, 'starknet_syncing')
-)
+export default starknetJsonRpc(bindings[Source.Juno_JsonRpc][0])
